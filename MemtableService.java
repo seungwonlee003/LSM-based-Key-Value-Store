37235402c8090e09034e397e47e95c9e44b70362
Seungwon Lee <118390973+seungwonlee003@users.noreply.github.com>
2025-04-19 14:28:50 +0900

    Update MemtableService.java

--- MemtableService.java
+++ MemtableService.java
@@ -1,1 +1,74 @@
+class MemtableService {
+    private final Config config;
+    private final ReadWriteLock rwLock = new ReentrantReadWriteLock();
+    private Memtable activeMemtable;
+    private final Queue<Memtable> flushQueue = new ArrayDeque<>();
 
+    public MemtableService(Config config) {
+        this.config = config;
+        this.activeMemtable = new Memtable();
+    }
+
+    public String get(String key) { 
+        rwLock.readLock().lock();
+        try{
+            String v = activeMemtable.get(key);
+            if (v != null) return v;
+
+            for (Memtable m : flushQueue) {
+                v = m.get(key);
+                if (v != null) return v;
+            }
+            return null;
+        } finally {
+            rwLock.readLock().unlock();
+        }
+    }
+    
+    public void put(String k, String v) {
+        rwLock.writeLock().lock();
+        try{
+            activeMemtable.put(key, value);
+            if (activeMemtable.size() >= config.getMemtableThresholdBytes()) {
+                rotateMemtable();
+            }
+        } finally {
+            rwLock.writeLock().unlock();
+        }
+    }
+    
+    public void delete(String key) {
+        rwLock.writeLock().lock();
+        try {
+            activeMemtable.put(key, null);
+            if (activeMemtable.size() >= config.getMemtableThresholdBytes()) {
+                rotateMemtable();
+            }
+        } finally {
+            rwLock.writeLock().unlock();
+        }    
+    }
+
+    private void rotateMemtable() {
+        flushQueue.add(activeMemtable);
+        activeMemtable = new Memtable();
+    }
+    
+    public Memtable pollFlushableMemtable() {
+        rwLock.writeLock().lock();
+        try {
+            return flushQueue.poll();
+        } finally {
+            rwLock.writeLock().unlock();
+        }
+    }
+
+    public boolean hasFlushableMemtable() {
+        rwLock.readLock().lock();
+        try {
+            return !flushQueue.isEmpty();
+        } finally {
+            rwLock.readLock().unlock();
+        }
+    }
+}
