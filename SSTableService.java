4f2d68b70a3d153acd70617937cc64106f5eaacb
Seungwon Lee <118390973+seungwonlee003@users.noreply.github.com>
2025-04-19 15:54:30 +0900

    Update SSTableService.java

--- SSTableService.java
+++ SSTableService.java
@@ -1,1 +1,28 @@
+public class SSTableService {
+    private final Manifest manifest;
+    private final long baseSegmentSize;
 
+    public SSTableService(Manifest manifest, long baseSegmentSize) {
+        this.manifest = Objects.requireNonNull(manifest);
+        this.baseSegmentSize = baseSegmentSize;
+    }
+
+    public void flush(Memtable memtable) {
+        SSTable sstable = SSTable.createFromMemtable(memtable, baseSegmentSize);
+        manifest.addSSTable(0, sstable);
+    }
+
+    public String get(String key) {
+        for (int level = 0; level <= manifest.maxLevel(); level++) {
+            for (SSTable sstable : manifest.getSSTables(level)) {
+                if (sstable.mightContain(key)) {
+                    String value = sstable.get(key);
+                    if (value != null) {
+                        return value;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+}
