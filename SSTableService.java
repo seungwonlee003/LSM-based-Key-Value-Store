bec88d52b24bc7c1e830e1ce135938cd6eb5d744
Seungwon Lee <118390973+seungwonlee003@users.noreply.github.com>
2025-04-20 12:32:54 +0900

    Update SSTableService.java

--- SSTableService.java
+++ SSTableService.java
@@ -1,10 +1,8 @@
 public class SSTableService {
     private final Manifest manifest;
-    private final long baseSegmentSize;
 
     public SSTableService(Manifest manifest, long baseSegmentSize) {
         this.manifest = Objects.requireNonNull(manifest);
-        this.baseSegmentSize = baseSegmentSize;
     }
 
     public String get(String key) {
