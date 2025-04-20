1c7dade2329447d0356ff2626069e96082edf48c
Seungwon Lee <118390973+seungwonlee003@users.noreply.github.com>
2025-04-20 14:02:09 +0900

    Update SSTableService.java

--- SSTableService.java
+++ SSTableService.java
@@ -1,19 +1,24 @@
 public class SSTableService {
     private final Manifest manifest;
 
-    public SSTableService(Manifest manifest, long baseSegmentSize) {
+    public SSTableService(Manifest manifest) {
         this.manifest = Objects.requireNonNull(manifest);
     }
 
     public String get(String key) {
-        for (int level = 0; level <= manifest.maxLevel(); level++) {
-            for (SSTable sstable : manifest.getSSTables(level)) {
-                String value = sstable.get(key);
-                if (value != null) {
-                    return value;
+        manifest.getLock().readLock().lock();
+        try {
+            for (int level = 0; level <= manifest.maxLevel(); level++) {
+                for (SSTable sstable : manifest.getSSTables(level)) {
+                    String value = sstable.get(key);
+                    if (value != null) {
+                        return value;
+                    }
                 }
             }
+            return null;
+        } finally {
+            manifest.getLock().readLock().unlock();
         }
-        return null;
     }
 }
