66a272eb3f7841091176865f84915f10313c8a37
Seungwon Lee <118390973+seungwonlee003@users.noreply.github.com>
2025-04-21 16:16:14 +0900

    Update DB.java

--- DB.java
+++ DB.java
@@ -8,12 +8,14 @@ public class DB {
         this.manifest = new Manifest();
         this.memtableService = new MemtableService(config);
         this.sstableService = new SSTableService(manifest);
-        this.compactionService = new CompactionService(memTableSvc, manifest, config);
+        this.compactionService = new CompactionService(memtableService, manifest, config);
     }
     
     public String get(String key) {
+        // first, search in mutable/immutable memtable
         String value = memtableService.get(key);
         if(value != null) return value;
+        // if not found, search in sstable
         return sstableService.get(key);
     }
 
